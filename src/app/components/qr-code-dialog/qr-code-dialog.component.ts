b709f1ecbe7d54914de226a95766a0a463a6e0a3
jirigalis <jiri@infinetcloud.com>
2025-04-10 15:03:57 +0200

    Generate QR code url from the base href value, add two more QR codes

--- src/app/components/qr-code-dialog/qr-code-dialog.component.ts
+++ src/app/components/qr-code-dialog/qr-code-dialog.component.ts
@@ -1,6 +1,7 @@
-import { Component, inject, signal } from '@angular/core';
+import { Component, Inject, inject, signal } from '@angular/core';
 import { MAT_DIALOG_DATA, MatDialogActions, MatDialogClose, MatDialogContent } from '@angular/material/dialog';
 import { MatButton } from '@angular/material/button';
+import { DOCUMENT } from '@angular/common';
 
 @Component({
   selector: 'app-qr-code-dialog',
@@ -17,8 +18,10 @@ export class QrCodeDialogComponent {
     qrCodeUrl = signal('');
     readonly data = inject(MAT_DIALOG_DATA);
 
-    constructor() {
-        const url = window.location.origin + '/assets/qr-codes/message-'+ this.data.messageNumber + '.png';
+    constructor(@Inject(DOCUMENT) private document: Document) {
+        const base = this.document.querySelector('base');
+        const baseUrl = base ? base.getAttribute('href') : '';
+        const url = window.location.origin + baseUrl + 'assets/qr-codes/message-'+ this.data.messageNumber + '.png';
         this.qrCodeUrl.set(url);
     }
 }
